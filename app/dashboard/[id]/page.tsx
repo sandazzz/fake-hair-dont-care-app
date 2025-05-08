a635bd7e08ef41ee04cc6c42182fd18df92410a0
sandazzz <rsandarisoa@gmail.com>
2025-05-08 03:26:25 +0200

    params id bug fixed

--- app/dashboard/[id]/page.tsx
+++ app/dashboard/[id]/page.tsx
@@ -2,7 +2,11 @@ import { DonationDetails } from "@/components/dashboard/donation-details";
 interface PageProps {
   params: { id: string };
 }
-export default async function DonationDetailsPage({ params }: PageProps) {
+export default async function DonationDetailsPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   const { id } = await params;
   return <DonationDetails id={id} />;
 }
