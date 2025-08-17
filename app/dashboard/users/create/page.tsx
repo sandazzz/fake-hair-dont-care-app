3afce343076539874a16a046d5a2f493db3729d1
sandazzz <rsandarisoa@gmail.com>
2025-08-18 01:59:19 +0200

    feat: implement user management features in dashboard
    - Added user listing page with a table displaying user details including name, email, role, and creation date.
    - Introduced a delete user functionality with a confirmation button.
    - Created a new user creation page with a form to input user details and submit to the API.
    - Enhanced navigation with links to create a new user and return to the user list.

---
+++ app/dashboard/users/create/page.tsx
@@ -0,0 +1,34 @@
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import Link from "next/link";
+
+export default async function page() {
+  return (
+    <div>
+      <h1>Créer un utilisateur</h1>
+      <Link href="/dashboard/users">Retour à la liste des utilisateurs</Link>
+    <form action={async (formData) => {
+        "use server";
+        const newUser = await auth.api.createUser({
+          body: {
+              email: formData.get("email") as string, // required
+              password: formData.get("password") as string, // required
+              name: formData.get("name") as string, // required
+              role: formData.get("role") as "admin" | "user",
+          },
+          headers: await headers(),
+      });
+      revalidatePath("/dashboard/users");
+      redirect("/dashboard/users");
+    }}>
+        <input type="email" name="email" placeholder="Email" />
+        <input type="password" name="password" placeholder="Password" />
+        <input type="text" name="name" placeholder="Name" />
+        <input type="text" name="role" placeholder="Role" />
+        <button type="submit">Create User</button>
+    </form>
+    </div>
+  )
+}
